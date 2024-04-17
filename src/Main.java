5edfa374cb55eabb396f0e330f8061e1101cbbcc
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-04-17 19:46:06 -0400

    we got a window working

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@ import github.tools.client.GitHubApiClient;
 import github.tools.responseObjects.*;
 import git.tools.client.GitSubprocessClient;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
         //GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
         //GitHubApiClient api = new GitHubApiClient(null, null);
+
+        JFrame window = new JFrame("GitRepoCreator");
+        window.setSize(400, 500);
+        window.setLayout(null);
+
+        // will show the window
+        window.setVisible(true);
     }
 }
